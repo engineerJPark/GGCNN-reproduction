d66917bd7d13f3e99a5ba78cb4c40fbcb6df4841
jshackist <jshackist@gmail.com>
2022-07-12 15:58:55 +0900

    GraspRectangle done

--- src/utils/processing/grasp.py
+++ src/utils/processing/grasp.py
@@ -1,3 +1,4 @@
+from curses.textpad import rectangle
 from re import X
 import numpy as np
 import matplotlib.pyplot as plt
@@ -153,15 +154,156 @@ class GraspRectangles:
         return np.mean(np.vstack([gr.points for gr in self.grs]),\
              axis=0).astype(np.int32)
 
+
 class GraspRectangle:
     '''
     representation of a grasp in the common 'Grasp Rectangle format'
     '''
     def __init__(self, points):
+        '''
+        points as gr.points N x 4 x 2
+        each 4 points has format as : [position, x/y axis]
+        '''
         self.points = points
 
     def __str__(self):
         return str(self.points)
+
+    @property
+    def as_grasp(self):
+        '''
+        return GraspRectangle converted to a Grasp
+        '''
+        return Grasp(self.center, self.angle, self.length, self.width)
+
+    @property
+    def center(self):
+        '''
+        return: rectangel center point
+        '''
+        return self.points.mean(axis=0).astype(np.int)
+    
+    @property
+    def length(self):
+        '''
+        return Rectangle length along the axis of the grasp
+        '''
+        dx = self.points[1,1] - self.points[0,1]
+        dy = self.points[1,0] - self.points[0,0]
+        return np.sqrt(dx**2 + dy**2)
+
+    @property
+    def width(self):
+        '''
+        return Rectangel width perpendicular to the axis of the grasp
+        '''
+        dx = self.points[2,1] - self.points[1,1]
+        dy = self.points[2,0] - self.points[1,0]
+        return np.sqrt(dx**2 + dy**2)
+
+    def polygon_coords(self, shape=None):
+        '''
+        shape : output shape
+        return indices of pixels, within the grasp rectangle polygon
+        '''
+        return polygon(self.points[:, 0], self.points[:, 1], shape)
+
+    def compact_polygon_coords(self, shape=None): 
+        '''
+        shape : output shape
+        return indices of pixels, within the centre third of the grasp rectangle
+        '''
+        # need to understand
+        return Grasp(self.center, self.angle, self.length/3, self.width).as_gr.polygon_coords(shape)
+
+    def iou(self, gr, angle_threshold=np.pi/6):
+        '''
+        conpute IoU with another grasping rectangle
+        gr : Grasping Rectangel to compare
+        angle_threshold : Maximum angle difference bw GraspRectangle
+        return IoU between Grasp Rectangles
+        '''
+        # need to understand
+        # self.angle이나 gr.angle이 90도 더 틀어져있나? 
+        if abs((self.angle - gr.angle + np.pi/2) % np.pi - np.pi/2) > angle_threshold: 
+            return 0
+
+        rr1, cc1 = self.polygon_coords()
+        rr2, cc2 = polygon(gr.points[:, 0], gr.points[:, 1])
+
+        try: # for drawing figure, get maximum indices
+            r_max = max(rr1.max(), rr2.max()) + 1
+            c_max = max(cc1.max(), cc2.max()) + 1
+        except:
+            return 0
+
+        canvas = np.zeros((r_max, c_max))
+        canvas[rr1, cc1] += 1
+        canvas[rr2, cc2] += 1
+        union = np.sum(canvas > 0)
+        if union == 0:
+            return 0
+        intersection = np.sum(canvas==2)
+        return intersection / union
+
+    def copy(self):
+        '''
+        return copy of itself
+        '''
+        return GraspRectangle(self.points.copy())
+
+    def offset(self, offset):
+        '''
+        offset grasp rectangle
+        offset : array [y, x] distance to offset
+        '''
+        self.points += np.array(offset).reshape((1,2))
+
+    def rotate(self, angle, center):
+        '''
+        angle : angle to rotate in radians
+        center : point to rotate around like image center
+        '''
+        R = np.array(
+            [
+                [np.cos(-angle), np.sin(-angle)],
+                [-1 * np.sin(-angle), np.cos(-angle)]
+            ]
+        )
+        c = np.array(center).reshape((1,2))
+        self.points = ((np.dot(R, (self.points - c).T)).T + c).astype(np.int32)
+
+    def scale(self, factor):
+        '''
+        factor : scale grasp rectangle by factor
+        '''
+        if factor == 1.:
+            return
+        self.points *= factor
+
+    def plot(self, ax, color=None):
+        '''
+        Plot grasping rectangle.
+        ax: Existing matplotlib axis
+        color: matplotlib color code (optional)
+        '''
+        points = np.vstack((self.points, self.points[0]))
+        ax.plot(points[:, 1], points[:, 0], color=color)
+
+    def zoom(self, factor, center):
+        '''
+        Zoom grasp rectangle by given factor.
+        factor: Zoom factor
+        center: Zoom zenter (focus point, e.g. image center)
+        '''
+        T = np.array(
+            [
+                [1/factor, 0],
+                [0, 1/factor]
+            ]
+        )
+        c = np.array(center).reshape((1, 2))
+        self.points = ((np.dot(T, (self.points - c).T)).T + c).astype(np.int)
 
 
 class Grasp:
